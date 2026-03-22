aaf8288543294e296f010f310d3de02d101802b9
Jaden Isaac <jadenisaac10@gmail.com>
2026-03-22 08:35:14 +0530

    Update Network.h

--- include/Network.h
+++ include/Network.h
@@ -9,6 +9,8 @@ public:
     Network(const std::vector<int>& layer_sizes);
     void predict(const Matrix& input, Matrix& output);
     void load_weights(const std::string& filename);
+    void train(const Matrix& input, const Matrix& label, double learning_rate);
+
 
 private:
     std::vector<Layer> layers;
